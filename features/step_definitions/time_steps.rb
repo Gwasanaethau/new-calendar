28e0fbeb993030002eba80083825709bea48dce4
Mark Pokorny <iridium193@gmail.com>
2015-06-09 22:21:22 +0100

    Reinstated first feature test.

--- features/step_definitions/time_steps.rb
+++ features/step_definitions/time_steps.rb
@@ -5,13 +5,13 @@ Given(/^a Unix timestamp of "([^"]*)"$/) do |unix_stamp|
 end
 
 Then(/^we see the short string of "([^"]*)"$/) do |output|
-  pending # expect(@calendar.print_short).to eql output
+  expect(@calendar.print_short).to eql output
 end
 
-Then(/^we see the unit string of "([^"]*)"$/) do |output|
+Then(/^we see the unit string of "([^"]*)"$/) do # |output|
   pending # expect(@calendar.print_unit).to eql output
 end
 
-Then(/^we see the long string of "([^"]*)"$/) do |output|
+Then(/^we see the long string of "([^"]*)"$/) do # |output|
   pending # expect(@calendar.print_long).to eql output
 end
