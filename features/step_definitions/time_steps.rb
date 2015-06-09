0882d1e4f668f6f54355a9820891f9a3ed691d6f
Mark Pokorny <iridium193@gmail.com>
2015-06-09 22:59:03 +0100

    Reinstated full feature test suite.

--- features/step_definitions/time_steps.rb
+++ features/step_definitions/time_steps.rb
@@ -8,10 +8,10 @@ Then(/^we see the short string of "([^"]*)"$/) do |output|
   expect(@calendar.print_short).to eql output
 end
 
-Then(/^we see the unit string of "([^"]*)"$/) do # |output|
-  pending # expect(@calendar.print_unit).to eql output
+Then(/^we see the unit string of "([^"]*)"$/) do |output|
+  expect(@calendar.print_unit).to eql output
 end
 
-Then(/^we see the long string of "([^"]*)"$/) do # |output|
-  pending # expect(@calendar.print_long).to eql output
+Then(/^we see the long string of "([^"]*)"$/) do |output|
+  expect(@calendar.print_long).to eql output
 end
